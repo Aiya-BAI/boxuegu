d3aacdd4f75452a91fb22d698e9eb2813808f796
baiyc <baiyc@superred.com.cn>
2022-07-15 17:26:24 +0800

    guanlixitong

---
+++ 1-3-面向对象/StudentManagerSystem/main.py
@@ -0,0 +1,16 @@
+class Student(object):
+    def __init__(self, name, gender, tel):
+        self.name = name
+        self.gender = gender
+        self.tel = tel
+
+    def __str__(self):
+        return f'{self.name}, {self.gender}, {self.tel}'
+
+
+
+
+
+
+# aa = Student('aa', 'nv', 333)
+# print(aa)
