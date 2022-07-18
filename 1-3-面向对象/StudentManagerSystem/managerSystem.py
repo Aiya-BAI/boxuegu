715bef0d661b79b0fa35d303a74429d4e3add204
baiyc <baiyc@superred.com.cn>
2022-07-18 18:53:44 +0800

    guanlixitong

---
+++ 1-3-面向对象/StudentManagerSystem/managerSystem.py
@@ -0,0 +1,50 @@
+class StudentManage(object):
+    def __init__(self):
+        # 存储学员数据
+        self.student_list = []
+
+    # 一、程序入口函数
+    def run(self):
+        # 加载文件里的数据
+        while True:
+
+            # 显示功能菜单
+
+            # 输入序号
+            menu_num = int(input('输入序号：'))
+            # 根据序号执行功能
+            if menu_num == 1:
+                # 添加
+                pass
+            elif menu_num == 1:
+                # 添加
+                pass
+            elif menu_num == 2:
+                # 删除
+                pass
+            elif menu_num == 3:
+                # 修改
+                pass
+            elif menu_num == 4:
+                # 查询
+                pass
+            elif menu_num == 5:
+                # 显示
+                pass
+            elif menu_num == 6:
+                # 保存
+                pass
+            elif menu_num == 7:
+                # 退出
+                break
+
+    # 二、系统功能函数
+    # 显示功能
+    # 添加
+    # 删除
+    # 修改
+    # 查询
+    # 显示
+    # 保存
+
+
