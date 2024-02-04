eb19b212e3f9eec9cc7e91925286c79f4e64b1c1
Aiya-baiyc <baiyc@superred.com.cn>
2024-02-04 16:14:28 +0800

    买单记账

--- 白月黑羽/模块与库/aa.py
+++ 白月黑羽/模块与库/aa.py
@@ -1,1 +1,15 @@
-fee = input()
+from save import savetofile
+
+fee = input('请输入午餐费用：')
+members = input('请输入聚餐人姓名，以中文逗号，分隔：')
+
+# 将人员放入一个列表
+memberlist = members.split('，')
+# 得到人数
+headcount = len(memberlist)
+
+avgfee = int(fee) / headcount
+print(avgfee)
+
+savetofile(memberlist, avgfee)
+
